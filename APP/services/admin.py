ce4f98bd6f0dacbb98fc957c5c254e008405c11a
brunomleguizamon <brunomleguizamon@gmail.com>
2021-06-15 14:42:31 -0300

    add urls

--- APP/services/admin.py
+++ APP/services/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import requests
+from .models import requests, CRM
 
 @admin.register(requests)
 class RequestAdmin(admin.ModelAdmin):
     list_display=('id_equipo', 'equipo', 'CRM', 'fecha_creacion', 'detalle', 'solicitante', 'area', 'sector', 'responsable_seguimiento')
     list_filter=('id_equipo', 'equipo', 'CRM', 'fecha_creacion', 'detalle', 'solicitante', 'area', 'sector', 'responsable_seguimiento')
+
+
+@admin.register(CRM)
+class CRMAdmin(admin.ModelAdmin):
+    list_display=('area', 'requerimiento', 'descripcion', 'prioridad')
+    list_filter=('area', 'requerimiento', 'descripcion', 'prioridad')
--- APP/services/models.py
+++ APP/services/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.db.models.fields.related import OneToOneField
 from django.utils import timezone
 from django.contrib.auth.models import User
+
 
 class requests (models.Model):
     
@@ -29,3 +31,37 @@ class requests (models.Model):
     responsable_seguimiento= models.ForeignKey(User,
                                                 on_delete=models.CASCADE)
     observaciones= models.TextField()
+
+
+class CRM (models.Model):
+
+    STATUS_AREA = (
+        ('binomio', 'Binomio'),
+        ('bioingeniria', 'Bioingenieria'),
+        ('centroQuirurgico', 'Centro Quirurgico'),
+        ('ConsExternos-Imagenes-Hemodinamia', 'Cons Externos Imagenes-Hemodinamia')
+    )
+
+    STATUS_REQUERIMIENTO = (
+        ('bioingenieria', 'Reparacion Bioingenieria'),
+        ('edilicio', 'Reparacion Edilicio')
+    )
+
+    STATUS_PRIORIDAD = (
+        ('alta', 'Alta'),
+        ('baja', 'Baja'), 
+    )
+
+    area = models.CharField(max_length=250,
+                            choices=STATUS_AREA,
+                            default='bioingenieria')
+    requerimiento = models.CharField(max_length=250,
+                                    choices=STATUS_REQUERIMIENTO,
+                                    default='edilicio')
+    descripcion= models.TextField()
+    prioridad= models.CharField(max_length=100,
+                                choices=STATUS_PRIORIDAD,
+                                default='baja')
+    creado_by= models.ForeignKey(User,
+                                on_delete=models.CASCADE)
+    
